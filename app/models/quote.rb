b7abf2e687d09feeeb355ab3700ec1dc12c48248
Steven Torrence <steve@cratebind.com>
2022-02-26 12:34:06 -0600

    Scope broadcast to company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,7 +4,7 @@ class Quote < ApplicationRecord
   belongs_to :company
 
   scope :ordered, -> { order(id: :desc) }
-  broadcasts_to ->(_quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   validates :name, presence: true
 end
